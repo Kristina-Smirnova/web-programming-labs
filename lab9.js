846e02df6b0d8741b712ef39e05fb564e0e13fc9
Kristina Smirnova <kristinasmirnova3791@gmail.com>
2022-12-23 09:47:54 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -11,3 +11,22 @@ function student(button) {
     else
         button.innerHTML = 'Смирнова';
 }
+
+function pushMe(button) {
+    if (button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'red';
+    }
+    else if (button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';
+        button.style.color = 'orange';
+    }  
+    else if (button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';
+           button.style.color = 'yellow'; 
+        }
+    else {
+        button.innerHTML = 'Нажми меня';
+        button.style.color = 'green';
+    }
+}
